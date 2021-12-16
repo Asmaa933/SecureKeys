6a52cb2ef2cffec05a9aac5245743aea395b2ed3
Asmaa <asmaatarek93@gmail.com>
2021-12-16 19:40:35 +0200

    Add label to center of view

--- SecureKeys/ViewController.swift
+++ SecureKeys/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var apiLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = .green
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addApiLabelToView()
     }
-
-
 }
 
+fileprivate extension ViewController {
+    
+    func addApiLabelToView() {
+        view.addSubview(apiLabel)
+        NSLayoutConstraint.activate([apiLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+                                     apiLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+                                     apiLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+                                    ])
+    }
+}
